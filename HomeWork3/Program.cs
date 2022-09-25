4ecfb552967aa85ef5fd1a15b525a4ca78eed557
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-09-25 12:48:20 +0300

    Add project files.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,49 @@
+﻿
+#region TASK2
+// int rem = 0, num = 0;
+//Console.WriteLine("Введите число: ");
+//int temp = Convert.ToInt32((Console.ReadLine()));
+//for ( int i =temp; i > 0; i = (i / 10))
+//{
+//    rem = i % 10;
+//    Console.Write(rem);
+
+//    num = (num * 10) + rem;
+
+//}
+
+
+//if (num == temp)
+//{
+//    Console.WriteLine(" Это число является палиндромом");
+//}
+//else
+//{
+//    Console.WriteLine("Число не является палиндромом");
+//}
+
+
+#endregion //TASK2
+
+#region TASK1
+//Console.Write("Введите ширину квадрата: ");
+//int n = Int32.Parse(Console.ReadLine());
+
+
+////Console.Write("введите код символа: ");
+////int num = int.Parse(Console.ReadLine());
+////Console.WriteLine("символ: " + (char)num);
+
+
+//var str = new String('*', n);
+//Console.WriteLine(str);
+//int i = 0;
+//while (i!=n-2)
+//{
+//    Console.WriteLine("*" + new string(' ', n - 2) + "*");
+//    i++;
+//}
+//Console.WriteLine(str);
+//Console.ReadLine();
+
+#endregion //TASK1
